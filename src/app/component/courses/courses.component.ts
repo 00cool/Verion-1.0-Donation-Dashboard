fdd2304da2d7edf01f410fb2d358570c18b2e3e5
jabbar memon <memonjabbar2@gmail.com>
2018-05-04 16:50:54 +0530

    changes in course component

--- src/app/component/courses/courses.component.ts
+++ src/app/component/courses/courses.component.ts
@@ -3,7 +3,7 @@ import * as moment from 'moment';
 import { AngularFirestoreCollection, AngularFirestore } from 'angularfire2/firestore';
 import { Observable } from 'rxjs/Observable';
 
-export interface course {course_name:string; course_desc : string; fee : number; location : string; start_date:Date; end_date: Date};
+export interface course {course_name:string; course_desc : string; fee : number; location : string; start_date:Date; end_date: Date; course_facilitator : string;course_outline :any[];about_course:string};
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
@@ -52,25 +52,48 @@ public courseCollection : AngularFirestoreCollection<course>;
     console.log(this.StartDate)
     var coursename = (document.getElementById('course_name') as HTMLInputElement).value;
     console.log(coursename);
-    
+    coursename.trim();
     var coursedec = (document.getElementById('course_desc') as HTMLInputElement).value;
     console.log(coursedec);
+    coursedec.trim();
     var fees = parseInt((document.getElementById('fee') as HTMLInputElement).value);
     console.log(fees);
     
     var locations = (document.getElementById('location') as HTMLInputElement).value;
     console.log(locations);
+    locations.trim();
+    var coursefac = (document.getElementById('facilitator') as HTMLInputElement).value;
+    console.log(coursefac);
+    coursefac.trim();
+
+    var aboutcourse = (document.getElementById('about') as HTMLInputElement).value;
+    console.log(aboutcourse);
+    aboutcourse.trim();
+
+    ///Remove the last dot from the string (Remaning)
+    var courseoutline =  (document.getElementById('desc') as HTMLInputElement).value;
+    courseoutline.substr(courseoutline.length-1);
+    console.log(courseoutline);
+    
+    var outline = courseoutline.split(".");
+    
+    console.log(outline);
+
+    outline.pop();
+    console.log(outline);
     this.finalstartdate = moment(this.StartDate).hours(this.StartTime.hour).minutes(this.StartTime.minute).toString();
     this.finalenddate = moment(this.EndDate).hours(this.EndTime.hour).minutes(this.EndTime.minute).toString();
 
     var dbdate = new Date(this.finalstartdate);
     var dbdate1 = new Date(this.finalenddate);
 
-    const item : course = {course_name :coursename, course_desc : coursedec , fee : fees , location : locations , start_date : dbdate , end_date : dbdate1}
+    const item : course = {course_name :coursename, course_desc : coursedec , fee : fees , location : locations , start_date : dbdate , end_date : dbdate1
+      ,about_course : aboutcourse ,course_facilitator : coursefac ,course_outline : outline}
     console.log(item);
-    // this.courseCollection.add(item).then((res) => {
-    //   console.log(res);
-    // })
+    this.courseCollection.add(item).then((res) => {
+      console.log(res);
+      location.reload(true);
+    })
   }
 
   show() {
@@ -99,6 +122,12 @@ public courseCollection : AngularFirestoreCollection<course>;
       (document.getElementById('course_desc') as HTMLInputElement).value = data.course_desc;
       (document.getElementById('fee') as HTMLInputElement).value = data.fee;
       (document.getElementById('location') as HTMLInputElement).value = data.location;
+      (document.getElementById('facilitator') as HTMLInputElement).value = data.course_facilitator;
+      (document.getElementById('desc') as HTMLInputElement).value = data.course_outline;
+      (document.getElementById('about') as HTMLInputElement).value = data.about_course;
+
+      
+
    
       var date_func = moment(data.start_date).format('MM/DD/YYYY');
       this.StartTime.hour = moment(data.start_date).hour();
@@ -150,6 +179,17 @@ public courseCollection : AngularFirestoreCollection<course>;
       const val2 = parseInt((document.getElementById('fee') as HTMLInputElement).value);
       const val3 = (document.getElementById('location') as HTMLInputElement).value;
       val3.trim();
+
+      var val4 = (document.getElementById('facilitator') as HTMLInputElement).value;
+      val4.trim();
+
+      var val5 = (document.getElementById('desc') as HTMLInputElement).value;
+      val5.trim();
+      var string1 = val5.split('.');
+      string1.pop();
+      var val6 = (document.getElementById('about') as HTMLInputElement).value;
+      val6.trim();
+
       // var db_date = new Date(this.StartDate + '' + this.StartTime );
       // console.log(db_date);
       // var db_date1 = new Date(this.EndDate  + '' + this.EndTime);
@@ -161,12 +201,14 @@ public courseCollection : AngularFirestoreCollection<course>;
       console.log('end ' + this.finalenddate);
       console.log('start ' + this.finalstartdate);
 
-      const item : course = {course_name:val , course_desc: val1 ,fee : val2 , location : val3 , start_date : this.finalstartdate ,end_date : this.finalenddate }
+     const item : course = {course_name:val , course_desc: val1 ,fee : val2 , location : val3 , start_date : this.finalstartdate ,end_date : this.finalenddate,
+    course_facilitator:val4 ,course_outline : string1,about_course : val6 }
       console.log(item);
       console.log(this.update_id);
       this.courseCollection.doc(this.update_id).update(item).then((res) => 
     {
       console.log(res);
+      location.reload(true);
     });
   }
   delete() {
